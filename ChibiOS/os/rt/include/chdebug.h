00af4bc73be8c1eabb4ff3adcec852f4a50a4250
Anurag Makineni <anurag.makineni@gmail.com>
2016-08-27 15:57:22 -0400

    add chibios

---
+++ ChibiOS/os/rt/include/chdebug.h
@@ -0,0 +1,366 @@
+/*
+    ChibiOS - Copyright (C) 2006..2015 Giovanni Di Sirio.
+
+    This file is part of ChibiOS.
+
+    ChibiOS is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    ChibiOS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * @file    chdebug.h
+ * @brief   Debug macros and structures.
+ *
+ * @addtogroup debug
+ * @{
+ */
+
+#ifndef _CHDEBUG_H_
+#define _CHDEBUG_H_
+
+/*===========================================================================*/
+/* Module constants.                                                         */
+/*===========================================================================*/
+
+/**
+ * @name    Trace record types
+ * @{
+ */
+#define CH_TRACE_TYPE_UNUSED                0U
+#define CH_TRACE_TYPE_SWITCH                1U
+#define CH_TRACE_TYPE_ISR_ENTER             2U
+#define CH_TRACE_TYPE_ISR_LEAVE             3U
+#define CH_TRACE_TYPE_HALT                  4U
+#define CH_TRACE_TYPE_USER                  5U
+/** @} */
+
+/**
+ * @name    Trace suspend masks
+ * @{
+ */
+#define CH_TRACE_SUSPEND_NONE               0U
+#define CH_TRACE_SUSPEND_SWITCH             (1U << CH_TRACE_TYPE_SWITCH)
+#define CH_TRACE_SUSPEND_ISR_ENTER          (1U << CH_TRACE_TYPE_ISR_ENTER)
+#define CH_TRACE_SUSPEND_ISR_LEAVE          (1U << CH_TRACE_TYPE_ISR_LEAVE)
+#define CH_TRACE_SUSPEND_HALT               (1U << CH_TRACE_TYPE_HALT)
+#define CH_TRACE_SUSPEND_USER               (1U << CH_TRACE_TYPE_USER)
+#define CH_TRACE_SUSPEND_ALL                (CH_TRACE_SUSPEND_SWITCH |      \
+                                             CH_TRACE_SUSPEND_ISR_ENTER |   \
+                                             CH_TRACE_SUSPEND_ISR_LEAVE |   \
+                                             CH_TRACE_SUSPEND_HALT |        \
+                                             CH_TRACE_SUSPEND_USER)
+
+/** @} */
+
+/**
+ * @name    Events to trace
+ * @{
+ */
+#define CH_DBG_TRACE_MASK_NONE              0U
+#define CH_DBG_TRACE_MASK_SWITCH            1U
+#define CH_DBG_TRACE_MASK_ISR               2U
+#define CH_DBG_TRACE_MASK_HALT              4U
+#define CH_DBG_TRACE_MASK_USER              8U
+#define CH_DBG_TRACE_MASK_ALL               (CH_DBG_TRACE_MASK_SWITCH |     \
+                                             CH_DBG_TRACE_MASK_ISR |        \
+                                             CH_DBG_TRACE_MASK_HALT |       \
+                                             CH_DBG_TRACE_MASK_USER)
+/** @} */
+
+/*===========================================================================*/
+/* Module pre-compile time settings.                                         */
+/*===========================================================================*/
+
+/**
+ * @name    Debug related settings
+ * @{
+ */
+/**
+ * @brief   Trace buffer entries.
+ */
+#if !defined(CH_DBG_TRACE_MASK) || defined(__DOXYGEN__)
+#define CH_DBG_TRACE_MASK                   CH_DBG_TRACE_MASK_ALL
+#endif
+
+/**
+ * @brief   Trace buffer entries.
+ * @note    The trace buffer is only allocated if @p CH_DBG_TRACE_MASK is
+ *          different from @p CH_DBG_TRACE_MASK_NONE.
+ */
+#if !defined(CH_DBG_TRACE_BUFFER_SIZE) || defined(__DOXYGEN__)
+#define CH_DBG_TRACE_BUFFER_SIZE            128
+#endif
+
+/**
+ * @brief   Fill value for thread stack area in debug mode.
+ */
+#if !defined(CH_DBG_STACK_FILL_VALUE) || defined(__DOXYGEN__)
+#define CH_DBG_STACK_FILL_VALUE             0x55
+#endif
+/** @} */
+
+/*===========================================================================*/
+/* Derived constants and error checks.                                       */
+/*===========================================================================*/
+
+#if !defined(CH_CFG_TRACE_HOOK)
+#error "CH_CFG_TRACE_HOOK not defined in chconf.h"
+#endif
+
+/*===========================================================================*/
+/* Module data structures and types.                                         */
+/*===========================================================================*/
+
+#if (CH_DBG_TRACE_MASK != CH_DBG_TRACE_MASK_NONE) || defined(__DOXYGEN__)
+/**
+ * @brief   Trace buffer record.
+ */
+typedef struct {
+  /**
+   * @brief   Record type.
+   */
+  uint32_t              type:3;
+  /**
+   * @brief   Switched out thread state.
+   */
+  uint32_t              state:5;
+  /**
+   * @brief   Accurate time stamp.
+   * @note    This field only available if the post supports
+   *          @p PORT_SUPPORTS_RT else it is set to zero.
+   */
+  uint32_t              rtstamp:24;
+  /**
+   * @brief   System time stamp of the switch event.
+   */
+  systime_t             time;
+  union {
+    /**
+     * @brief   Structure representing a  context switch.
+     */
+    struct {
+      /**
+       * @brief   Switched in thread.
+       */
+      thread_t              *ntp;
+      /**
+       * @brief   Object where going to sleep.
+       */
+      void                  *wtobjp;
+    } sw;
+    /**
+     * @brief   Structure representing an ISR enter.
+     */
+    struct {
+      /**
+       * @brief   ISR function name taken using @p __func__.
+       */
+      const char            *name;
+    } isr;
+    /**
+     * @brief   Structure representing an halt.
+     */
+    struct {
+      /**
+       * @brief   Halt error string.
+       */
+      const char            *reason;
+    } halt;
+    /**
+     * @brief   User trace structure.
+     */
+    struct {
+      /**
+       * @brief   Trace user parameter 1.
+       */
+      void                  *up1;
+      /**
+       * @brief   Trace user parameter 2.
+       */
+      void                  *up2;
+    } user;
+  } u;
+} ch_trace_event_t;
+
+/**
+ * @brief   Trace buffer header.
+ */
+typedef struct {
+  /**
+   * @brief   Suspended trace sources mask.
+   */
+  uint16_t              suspended;
+  /**
+   * @brief   Trace buffer size (entries).
+   */
+  uint16_t              size;
+  /**
+   * @brief   Pointer to the buffer front.
+   */
+  ch_trace_event_t      *ptr;
+  /**
+   * @brief   Ring buffer.
+   */
+  ch_trace_event_t      buffer[CH_DBG_TRACE_BUFFER_SIZE];
+} ch_trace_buffer_t;
+#endif /* CH_DBG_TRACE_MASK != CH_DBG_TRACE_MASK_NONE */
+
+/*===========================================================================*/
+/* Module macros.                                                            */
+/*===========================================================================*/
+
+#if CH_DBG_SYSTEM_STATE_CHECK == TRUE
+#define _dbg_enter_lock() (ch.dbg.lock_cnt = (cnt_t)1)
+#define _dbg_leave_lock() (ch.dbg.lock_cnt = (cnt_t)0)
+#endif
+
+/* When the state checker feature is disabled then the following functions
+   are replaced by an empty macro.*/
+#if CH_DBG_SYSTEM_STATE_CHECK == FALSE
+#define _dbg_enter_lock()
+#define _dbg_leave_lock()
+#define _dbg_check_disable()
+#define _dbg_check_suspend()
+#define _dbg_check_enable()
+#define _dbg_check_lock()
+#define _dbg_check_unlock()
+#define _dbg_check_lock_from_isr()
+#define _dbg_check_unlock_from_isr()
+#define _dbg_check_enter_isr()
+#define _dbg_check_leave_isr()
+#define chDbgCheckClassI()
+#define chDbgCheckClassS()
+#endif
+
+/* When a trace feature is disabled the associated functions are replaced by
+   an empty macro.*/
+#if (CH_DBG_TRACE_MASK & CH_DBG_TRACE_MASK_SWITCH) == 0
+#define _dbg_trace_switch(otp)
+#endif
+#if (CH_DBG_TRACE_MASK & CH_DBG_TRACE_MASK_ISR) == 0
+#define _dbg_trace_isr_enter(isr)
+#define _dbg_trace_isr_leave(isr)
+#endif
+#if (CH_DBG_TRACE_MASK & CH_DBG_TRACE_MASK_HALT) == 0
+#define _dbg_trace_halt(reason)
+#endif
+#if (CH_DBG_TRACE_MASK & CH_DBG_TRACE_MASK_USER) == 0
+#define chDbgWriteTraceI(up1, up2)
+#define chDbgWriteTrace(up1, up2)
+#endif
+
+/**
+ * @name    Macro Functions
+ * @{
+ */
+/**
+ * @brief   Function parameters check.
+ * @details If the condition check fails then the kernel panics and halts.
+ * @note    The condition is tested only if the @p CH_DBG_ENABLE_CHECKS switch
+ *          is specified in @p chconf.h else the macro does nothing.
+ *
+ * @param[in] c         the condition to be verified to be true
+ *
+ * @api
+ */
+#if !defined(chDbgCheck)
+#define chDbgCheck(c) do {                                                  \
+  /*lint -save -e506 -e774 [2.1, 14.3] Can be a constant by design.*/       \
+  if (CH_DBG_ENABLE_CHECKS != FALSE) {                                      \
+    if (!(c)) {                                                             \
+  /*lint -restore*/                                                         \
+      chSysHalt(__func__);                                                  \
+    }                                                                       \
+  }                                                                         \
+} while (false)
+#endif /* !defined(chDbgCheck) */
+
+/**
+ * @brief   Condition assertion.
+ * @details If the condition check fails then the kernel panics with a
+ *          message and halts.
+ * @note    The condition is tested only if the @p CH_DBG_ENABLE_ASSERTS switch
+ *          is specified in @p chconf.h else the macro does nothing.
+ * @note    The remark string is not currently used except for putting a
+ *          comment in the code about the assertion.
+ *
+ * @param[in] c         the condition to be verified to be true
+ * @param[in] r         a remark string
+ *
+ * @api
+ */
+#if !defined(chDbgAssert)
+#define chDbgAssert(c, r) do {                                              \
+  /*lint -save -e506 -e774 [2.1, 14.3] Can be a constant by design.*/       \
+  if (CH_DBG_ENABLE_ASSERTS != FALSE) {                                     \
+    if (!(c)) {                                                             \
+  /*lint -restore*/                                                         \
+      chSysHalt(__func__);                                                  \
+    }                                                                       \
+  }                                                                         \
+} while (false)
+#endif /* !defined(chDbgAssert) */
+/** @} */
+
+/*===========================================================================*/
+/* External declarations.                                                    */
+/*===========================================================================*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#if CH_DBG_SYSTEM_STATE_CHECK == TRUE
+  void _dbg_check_disable(void);
+  void _dbg_check_suspend(void);
+  void _dbg_check_enable(void);
+  void _dbg_check_lock(void);
+  void _dbg_check_unlock(void);
+  void _dbg_check_lock_from_isr(void);
+  void _dbg_check_unlock_from_isr(void);
+  void _dbg_check_enter_isr(void);
+  void _dbg_check_leave_isr(void);
+  void chDbgCheckClassI(void);
+  void chDbgCheckClassS(void);
+#endif
+#if (CH_DBG_TRACE_MASK != CH_DBG_TRACE_MASK_NONE) || defined(__DOXYGEN__)
+  void _dbg_trace_init(void);
+#if (CH_DBG_TRACE_MASK & CH_DBG_TRACE_MASK_SWITCH) != 0
+  void _dbg_trace_switch(thread_t *otp);
+#endif
+#if (CH_DBG_TRACE_MASK & CH_DBG_TRACE_MASK_ISR) != 0
+  void _dbg_trace_isr_enter(const char *isr);
+  void _dbg_trace_isr_leave(const char *isr);
+#endif
+#if (CH_DBG_TRACE_MASK & CH_DBG_TRACE_MASK_HALT) != 0
+  void _dbg_trace_halt(const char *reason);
+#endif
+#if (CH_DBG_TRACE_MASK & CH_DBG_TRACE_MASK_USER) != 0
+  void chDbgWriteTraceI(void *up1, void *up2);
+  void chDbgWriteTrace(void *up1, void *up2);
+  void chDbgSuspendTraceI(uint16_t mask);
+  void chDbgSuspendTrace(uint16_t mask);
+  void chDbgResumeTraceI(uint16_t mask);
+  void chDbgResumeTrace(uint16_t mask);
+#endif
+#endif /* CH_DBG_TRACE_MASK != CH_DBG_TRACE_MASK_NONE */
+#ifdef __cplusplus
+}
+#endif
+
+/*===========================================================================*/
+/* Module inline functions.                                                  */
+/*===========================================================================*/
+
+#endif /* _CHDEBUG_H_ */
+
+/** @} */
---
+++ ChibiOS/os/rt/src/chdebug.c
@@ -0,0 +1,478 @@
+/*
+    ChibiOS - Copyright (C) 2006..2015 Giovanni Di Sirio.
+
+    This file is part of ChibiOS.
+
+    ChibiOS is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    ChibiOS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * @file    chdebug.c
+ * @brief   ChibiOS/RT Debug code.
+ *
+ * @addtogroup debug
+ * @details Debug APIs and services:
+ *          - Runtime system state and call protocol check. The following
+ *            panic messages can be generated:
+ *            - SV#1, misplaced @p chSysDisable().
+ *              - Called from an ISR.
+ *              - Called from a critical zone.
+ *              .
+ *            - SV#2, misplaced @p chSysSuspend()
+ *              - Called from an ISR.
+ *              - Called from a critical zone.
+ *              .
+ *            - SV#3, misplaced @p chSysEnable().
+ *              - Called from an ISR.
+ *              - Called from a critical zone.
+ *              .
+ *            - SV#4, misplaced @p chSysLock().
+ *              - Called from an ISR.
+ *              - Called from a critical zone.
+ *              .
+ *            - SV#5, misplaced @p chSysUnlock().
+ *              - Called from an ISR.
+ *              - Not called from a critical zone.
+ *              .
+ *            - SV#6, misplaced @p chSysLockFromISR().
+ *              - Not called from an ISR.
+ *              - Called from a critical zone.
+ *              .
+ *            - SV#7, misplaced @p chSysUnlockFromISR().
+ *              - Not called from an ISR.
+ *              - Not called from a critical zone.
+ *              .
+ *            - SV#8, misplaced @p CH_IRQ_PROLOGUE().
+ *              - Not called at ISR begin.
+ *              - Called from a critical zone.
+ *              .
+ *            - SV#9, misplaced @p CH_IRQ_EPILOGUE().
+ *              - @p CH_IRQ_PROLOGUE() missing.
+ *              - Not called at ISR end.
+ *              - Called from a critical zone.
+ *              .
+ *            - SV#10, misplaced I-class function.
+ *              - I-class function not called from within a critical zone.
+ *              .
+ *            - SV#11, misplaced S-class function.
+ *              - S-class function not called from within a critical zone.
+ *              - Called from an ISR.
+ *            .
+ *          - Trace buffer.
+ *          - Parameters check.
+ *          - Kernel assertions.
+ *          - Kernel panics.
+ *          .
+ * @note    Stack checks are not implemented in this module but in the port
+ *          layer in an architecture-dependent way.
+ * @{
+ */
+
+#include "ch.h"
+
+/*===========================================================================*/
+/* Module local definitions.                                                 */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Module exported variables.                                                */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Module local types.                                                       */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Module local variables.                                                   */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Module local functions.                                                   */
+/*===========================================================================*/
+
+#if (CH_DBG_TRACE_MASK != CH_DBG_TRACE_MASK_NONE) || defined(__DOXYGEN__)
+/**
+ * @brief   Writes a time stamp and increases the trace buffer pointer.
+ *
+ * @notapi
+ */
+NOINLINE static void trace_next(void) {
+
+  ch.dbg.trace_buffer.ptr->time    = chVTGetSystemTimeX();
+#if PORT_SUPPORTS_RT == TRUE
+  ch.dbg.trace_buffer.ptr->rtstamp = chSysGetRealtimeCounterX();
+#else
+  ch.dbg.trace_buffer.ptr->rtstamp = (rtcnt_t)0;
+#endif
+
+  /* Trace hook, useful in order to interface debug tools.*/
+  CH_CFG_TRACE_HOOK(ch.dbg.trace_buffer.ptr);
+
+  if (++ch.dbg.trace_buffer.ptr >=
+      &ch.dbg.trace_buffer.buffer[CH_DBG_TRACE_BUFFER_SIZE]) {
+    ch.dbg.trace_buffer.ptr = &ch.dbg.trace_buffer.buffer[0];
+  }
+}
+#endif
+
+/*===========================================================================*/
+/* Module exported functions.                                                */
+/*===========================================================================*/
+
+#if (CH_DBG_SYSTEM_STATE_CHECK == TRUE) || defined(__DOXYGEN__)
+/**
+ * @brief   Guard code for @p chSysDisable().
+ *
+ * @notapi
+ */
+void _dbg_check_disable(void) {
+
+  if ((ch.dbg.isr_cnt != (cnt_t)0) || (ch.dbg.lock_cnt != (cnt_t)0)) {
+    chSysHalt("SV#1");
+  }
+}
+
+/**
+ * @brief   Guard code for @p chSysSuspend().
+ *
+ * @notapi
+ */
+void _dbg_check_suspend(void) {
+
+  if ((ch.dbg.isr_cnt != (cnt_t)0) || (ch.dbg.lock_cnt != (cnt_t)0)) {
+    chSysHalt("SV#2");
+  }
+}
+
+/**
+ * @brief   Guard code for @p chSysEnable().
+ *
+ * @notapi
+ */
+void _dbg_check_enable(void) {
+
+  if ((ch.dbg.isr_cnt != (cnt_t)0) || (ch.dbg.lock_cnt != (cnt_t)0)) {
+    chSysHalt("SV#3");
+  }
+}
+
+/**
+ * @brief   Guard code for @p chSysLock().
+ *
+ * @notapi
+ */
+void _dbg_check_lock(void) {
+
+  if ((ch.dbg.isr_cnt != (cnt_t)0) || (ch.dbg.lock_cnt != (cnt_t)0)) {
+    chSysHalt("SV#4");
+  }
+  _dbg_enter_lock();
+}
+
+/**
+ * @brief   Guard code for @p chSysUnlock().
+ *
+ * @notapi
+ */
+void _dbg_check_unlock(void) {
+
+  if ((ch.dbg.isr_cnt != (cnt_t)0) || (ch.dbg.lock_cnt <= (cnt_t)0)) {
+    chSysHalt("SV#5");
+  }
+  _dbg_leave_lock();
+}
+
+/**
+ * @brief   Guard code for @p chSysLockFromIsr().
+ *
+ * @notapi
+ */
+void _dbg_check_lock_from_isr(void) {
+
+  if ((ch.dbg.isr_cnt <= (cnt_t)0) || (ch.dbg.lock_cnt != (cnt_t)0)) {
+    chSysHalt("SV#6");
+  }
+  _dbg_enter_lock();
+}
+
+/**
+ * @brief   Guard code for @p chSysUnlockFromIsr().
+ *
+ * @notapi
+ */
+void _dbg_check_unlock_from_isr(void) {
+
+  if ((ch.dbg.isr_cnt <= (cnt_t)0) || (ch.dbg.lock_cnt <= (cnt_t)0)) {
+    chSysHalt("SV#7");
+  }
+  _dbg_leave_lock();
+}
+
+/**
+ * @brief   Guard code for @p CH_IRQ_PROLOGUE().
+ *
+ * @notapi
+ */
+void _dbg_check_enter_isr(void) {
+
+  port_lock_from_isr();
+  if ((ch.dbg.isr_cnt < (cnt_t)0) || (ch.dbg.lock_cnt != (cnt_t)0)) {
+    chSysHalt("SV#8");
+  }
+  ch.dbg.isr_cnt++;
+  port_unlock_from_isr();
+}
+
+/**
+ * @brief   Guard code for @p CH_IRQ_EPILOGUE().
+ *
+ * @notapi
+ */
+void _dbg_check_leave_isr(void) {
+
+  port_lock_from_isr();
+  if ((ch.dbg.isr_cnt <= (cnt_t)0) || (ch.dbg.lock_cnt != (cnt_t)0)) {
+    chSysHalt("SV#9");
+  }
+  ch.dbg.isr_cnt--;
+  port_unlock_from_isr();
+}
+
+/**
+ * @brief   I-class functions context check.
+ * @details Verifies that the system is in an appropriate state for invoking
+ *          an I-class API function. A panic is generated if the state is
+ *          not compatible.
+ *
+ * @api
+ */
+void chDbgCheckClassI(void) {
+
+  if ((ch.dbg.isr_cnt < (cnt_t)0) || (ch.dbg.lock_cnt <= (cnt_t)0)) {
+    chSysHalt("SV#10");
+  }
+}
+
+/**
+ * @brief   S-class functions context check.
+ * @details Verifies that the system is in an appropriate state for invoking
+ *          an S-class API function. A panic is generated if the state is
+ *          not compatible.
+ *
+ * @api
+ */
+void chDbgCheckClassS(void) {
+
+  if ((ch.dbg.isr_cnt != (cnt_t)0) || (ch.dbg.lock_cnt <= (cnt_t)0)) {
+    chSysHalt("SV#11");
+  }
+}
+
+#endif /* CH_DBG_SYSTEM_STATE_CHECK == TRUE */
+
+#if (CH_DBG_TRACE_MASK != CH_DBG_TRACE_MASK_NONE) || defined(__DOXYGEN__)
+/**
+ * @brief   Trace circular buffer subsystem initialization.
+ * @note    Internal use only.
+ */
+void _dbg_trace_init(void) {
+  unsigned i;
+
+  ch.dbg.trace_buffer.suspended = 0U;
+  ch.dbg.trace_buffer.size      = CH_DBG_TRACE_BUFFER_SIZE;
+  ch.dbg.trace_buffer.ptr       = &ch.dbg.trace_buffer.buffer[0];
+  for (i = 0U; i < CH_DBG_TRACE_BUFFER_SIZE; i++) {
+    ch.dbg.trace_buffer.buffer[i].type = CH_TRACE_TYPE_UNUSED;
+  }
+}
+
+#if ((CH_DBG_TRACE_MASK & CH_DBG_TRACE_MASK_SWITCH) != 0) ||                \
+    defined(__DOXYGEN__)
+/**
+ * @brief   Inserts in the circular debug trace buffer a context switch record.
+ *
+ * @param[in] otp       the thread being switched out
+ *
+ * @notapi
+ */
+void _dbg_trace_switch(thread_t *otp) {
+
+  if ((ch.dbg.trace_buffer.suspended & CH_TRACE_SUSPEND_SWITCH) == 0U) {
+    ch.dbg.trace_buffer.ptr->type        = CH_TRACE_TYPE_SWITCH;
+    ch.dbg.trace_buffer.ptr->state       = (uint8_t)otp->state;
+    ch.dbg.trace_buffer.ptr->u.sw.ntp    = currp;
+    ch.dbg.trace_buffer.ptr->u.sw.wtobjp = otp->u.wtobjp;
+    trace_next();
+  }
+}
+#endif /* (CH_DBG_TRACE_MASK & CH_DBG_TRACE_MASK_SWITCH) != 0 */
+
+#if ((CH_DBG_TRACE_MASK & CH_DBG_TRACE_MASK_ISR) != 0) ||                   \
+    defined(__DOXYGEN__)
+/**
+ * @brief   Inserts in the circular debug trace buffer an ISR-enter record.
+ *
+ * @param[in] isr       name of the isr
+ *
+ * @notapi
+ */
+void _dbg_trace_isr_enter(const char *isr) {
+
+  if ((ch.dbg.trace_buffer.suspended & CH_TRACE_SUSPEND_ISR_ENTER) == 0U) {
+    port_lock_from_isr();
+    ch.dbg.trace_buffer.ptr->type        = CH_TRACE_TYPE_ISR_ENTER;
+    ch.dbg.trace_buffer.ptr->state       = 0U;
+    ch.dbg.trace_buffer.ptr->u.isr.name  = isr;
+    trace_next();
+    port_unlock_from_isr();
+  }
+}
+
+/**
+ * @brief   Inserts in the circular debug trace buffer an ISR-leave record.
+ *
+ * @param[in] isr       name of the isr
+ *
+ * @notapi
+ */
+void _dbg_trace_isr_leave(const char *isr) {
+
+  if ((ch.dbg.trace_buffer.suspended & CH_TRACE_SUSPEND_ISR_LEAVE) == 0U) {
+    port_lock_from_isr();
+    ch.dbg.trace_buffer.ptr->type        = CH_TRACE_TYPE_ISR_LEAVE;
+    ch.dbg.trace_buffer.ptr->state       = 0U;
+    ch.dbg.trace_buffer.ptr->u.isr.name  = isr;
+    trace_next();
+    port_unlock_from_isr();
+  }
+}
+#endif /* (CH_DBG_TRACE_MASK & CH_DBG_TRACE_MASK_ISR) != 0 */
+
+#if ((CH_DBG_TRACE_MASK & CH_DBG_TRACE_MASK_HALT) != 0) ||                  \
+    defined(__DOXYGEN__)
+/**
+ * @brief   Inserts in the circular debug trace buffer an halt record.
+ *
+ * @param[in] reason    the halt error string
+ *
+ * @notapi
+ */
+void _dbg_trace_halt(const char *reason) {
+
+  if ((ch.dbg.trace_buffer.suspended & CH_TRACE_SUSPEND_HALT) == 0U) {
+    ch.dbg.trace_buffer.ptr->type          = CH_TRACE_TYPE_HALT;
+    ch.dbg.trace_buffer.ptr->state         = 0;
+    ch.dbg.trace_buffer.ptr->u.halt.reason = reason;
+    trace_next();
+  }
+}
+#endif /* (CH_DBG_TRACE_MASK & CH_DBG_TRACE_MASK_HALT) != 0 */
+
+#if ((CH_DBG_TRACE_MASK & CH_DBG_TRACE_MASK_USER) != 0) ||                  \
+    defined(__DOXYGEN__)
+/**
+ * @brief   Adds an user trace record to the trace buffer.
+ *
+ * @param[in] up1       user parameter 1
+ * @param[in] up2       user parameter 2
+ *
+ * @iclass
+ */
+void chDbgWriteTraceI(void *up1, void *up2) {
+
+  chDbgCheckClassI();
+
+  if ((ch.dbg.trace_buffer.suspended & CH_TRACE_SUSPEND_SWITCH) == 0U) {
+    ch.dbg.trace_buffer.ptr->type       = CH_TRACE_TYPE_USER;
+    ch.dbg.trace_buffer.ptr->state      = 0;
+    ch.dbg.trace_buffer.ptr->u.user.up1 = up1;
+    ch.dbg.trace_buffer.ptr->u.user.up2 = up2;
+    trace_next();
+  }
+}
+
+/**
+ * @brief   Adds an user trace record to the trace buffer.
+ *
+ * @param[in] up1       user parameter 1
+ * @param[in] up2       user parameter 2
+ *
+ * @api
+ */
+void chDbgWriteTrace(void *up1, void *up2) {
+
+  chSysLock();
+  chDbgWriteTraceI(up1, up2);
+  chSysUnlock();
+}
+#endif /* (CH_DBG_TRACE_MASK & CH_DBG_TRACE_MASK_USER) != 0 */
+
+/**
+ * @brief   Suspends one or more trace events.
+ *
+ * @paramin mask        mask of the trace events to be suspended
+ *
+ * @iclass
+ */
+void chDbgSuspendTraceI(uint16_t mask) {
+
+  chDbgCheckClassI();
+
+  ch.dbg.trace_buffer.suspended |= mask;
+}
+
+/**
+ * @brief   Suspends one or more trace events.
+ *
+ * @paramin mask        mask of the trace events to be suspended
+ *
+ * @api
+ */
+void chDbgSuspendTrace(uint16_t mask) {
+
+  chSysLock();
+  chDbgSuspendTraceI(mask);
+  chSysUnlock();
+}
+
+/**
+ * @brief   Resumes one or more trace events.
+ *
+ * @paramin mask        mask of the trace events to be resumed
+ *
+ * @iclass
+ */
+void chDbgResumeTraceI(uint16_t mask) {
+
+  chDbgCheckClassI();
+
+  ch.dbg.trace_buffer.suspended &= ~mask;
+}
+
+/**
+ * @brief   Resumes one or more trace events.
+ *
+ * @paramin mask        mask of the trace events to be resumed
+ *
+ * @api
+ */
+void chDbgResumeTrace(uint16_t mask) {
+
+  chSysLock();
+  chDbgResumeTraceI(mask);
+  chSysUnlock();
+}
+#endif /* CH_DBG_TRACE_MASK != CH_DBG_TRACE_MASK_NONE */
+
+/** @} */
