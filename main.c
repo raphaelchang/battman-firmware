e19244cfe7eab1e38eec910e8117932cff09a9b0
Anurag Makineni <anurag.makineni@gmail.com>
2016-09-29 23:19:42 -0400

    usb files

--- main.c
+++ main.c
@@ -1,29 +1,25 @@
-/*
-    ChibiOS - Copyright (C) 2006..2015 Giovanni Di Sirio
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-*/
-
 #include "ch.h"
 #include "hal.h"
 #include "gpio.h"
+#include "usbcfg.h"
 #include "hw_conf.h"
+
+#define usb_lld_connect_bus(usbp)
+#define usb_lld_disconnect_bus(usbp)
 
 int main(void) {
 
     halInit();
     chSysInit();
     gpio_init();
+
+    sduObjectInit(&SDU1);
+    sduStart(&SDU1, &serusbcfg);
+
+    usbDisconnectBus(serusbcfg.usbp);
+    chThdSleepMilliseconds(1500);
+    usbStart(serusbcfg.usbp, &usbcfg);
+    usbConnectBus(serusbcfg.usbp)
 
     while(1)
     {
